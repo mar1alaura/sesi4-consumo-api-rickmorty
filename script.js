36efead3906dcd7a50672b618141ba592751462f
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2025-08-20 10:50:12 -0300

    js pronto

--- script.js
+++ script.js
@@ -8,14 +8,40 @@ async function fetchCharacters(page){
     resultsDiv.innerHTML = "<p>Carregando...</p>"
 
     try {
-        const response = await fetch(`https://rickandmortyapi.com/api/character`)
+        const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
         const data = await response.json()
-        console.log(data)
+        //console.log(data)
+
+        if(data.error){
+            resultsDiv.innerHTML = "<p>Página inválida!</p>"
+            return
+        }
+
+        resultsDiv.innerHTML = "";
+        data.results.forEach(character => {
+            const card = document.createElement("div")
+            card.className = "card"
+            card.innerHTML = `
+            <img src="${character.image}" alt="${character.name}">
+            <h3>${character.name}</h3>
+            <p><strong>Status:</strong> ${character.status}</p>
+            <p><strong>Espécie:</strong> ${character.species}</p>
+            `
+            resultsDiv.appendChild(card)
+        });
+
     } catch (error) {
         resultsDiv.innerHTML = "<p>Erro ao buscar personagens!</p>"
     }
 }
 
-
+searchBtn.addEventListener("click", () => {
+ const page = pageInput.value.trim()  
+ if(page){
+    fetchCharacters(page)
+ }else{
+    resultsDiv.innerHTML = "<p>Digite um número de pagina!</p>"
+ }
+})
 
 fetchCharacters(1)
